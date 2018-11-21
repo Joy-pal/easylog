05c6a13d5518cdaaeed397ab3068aa61c2f02483
joy pal <joypal.jkp@yahoo.com>
2018-11-21 20:46:34 +0600

    Initial commit

---
+++ logutill/src/main/java/logutill/plugin/joy/com/logutill/LogUtill.java
@@ -0,0 +1,13 @@
+package logutill.plugin.joy.com.logutill;
+
+import android.util.Log;
+
+public class LogUtill {
+
+
+    public static final String TAG="Log Utill";
+
+    public static void d(String Message){
+        Log.d(TAG,Message);
+    }
+}
